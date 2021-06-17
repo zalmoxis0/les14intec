24766fba7ad701dd75ec656cc6d8fdb8f991694e
zalmoxis0 <66913338+zalmoxis0@users.noreply.github.com>
2021-06-17 20:26:21 +0200

    Add files via upload

---
+++ les14/src/com/company/ComplexTypes.java
@@ -0,0 +1,30 @@
+package com.company;
+
+import java.util.Scanner;
+
+import java.util.Scanner;
+
+public class ComplexTypes {
+    public static void main(String[] args) {
+
+        // Print
+        String welcome = "Welcome to somewhere..!";
+        System.out.println(welcome);
+
+        // memmory : declare a text data
+
+        //print
+        Scanner scanner = new Scanner(System.in);
+
+        int n1 = 10;
+        int n2 = 20;
+        int n3 = 30;
+        int n4 = 40;
+        int n5 = 50;
+
+        double n6 = 60.60;
+        double n7 = 70.70;
+        double n8 = 80.80;
+        System.out.println("Mumber 1 is " + n1);
+    }
+}
---
+++ les14/src/com/company/casino.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class casino {
+    public static void main(String[] args) {
+        System.out.println("Welcome to casino");
+
+        // Declare n1 to n4
+        int n1 , n2, n3, n4;
+    }
+}
